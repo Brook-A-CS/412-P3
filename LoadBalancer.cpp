e79376c2b477a749ba471c544d6a6b4fee722cf2
Brook-A-CS <brooka048@gmail.com>
2026-03-01 15:37:53 -0600

    load balancer core logic

---
+++ LoadBalancer.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include "LoadBalancer.h"
+#include "WebServer.h"
+#include "Request.h"
+#include "Color.h"
+
+const int WAIT_BETWEEN_REBALANCING = 30;
+
+LoadBalancer::LoadBalancer(int numServers) {
+    servers.resize(numServers);
+}
+
+void LoadBalancer::addRequest(Request r) {
+    requestQueue.push(r);
+}
+
+void LoadBalancer::increment() {
+    for (WebServer& server : servers) {
+        if (server.isFree() && !requestQueue.empty()) {
+            server.processRequest(std::move(requestQueue.front()));
+            requestQueue.pop();
+        }
+        server.tick();
+    }
+
+    if (currentTime % WAIT_BETWEEN_REBALANCING == 0) balanceLoad();
+    currentTime += 1;
+}
+
+void LoadBalancer::balanceLoad() {
+    if(requestQueue.size() > 80 * servers.size()) {
+        
+        servers.push_back(WebServer());
+        std::cout << Color::GREEN << "[INFO] " << Color::RESET <<
+        "Num servers increased by 1 for a total of " << server_size() << std::endl;
+    }
+
+    if(requestQueue.size() < 50 * servers.size()) {
+        servers.pop_back();
+
+        std::cout << Color::GREEN << "[INFO] " << Color::RESET <<
+        "Num servers increased by 1 for a total of " << server_size() << std::endl;
+    };
+
+    
+}
